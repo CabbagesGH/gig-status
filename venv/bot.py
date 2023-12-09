0db7f7ad376d36ccdd320af2bfed68a029ea4d66
CabbagesGH <mtiff2009@gmail.com>
2023-12-09 16:00:56 +0000

    Removed token

--- venv/bot.py
+++ venv/bot.py
@@ -6,7 +6,9 @@ async def send_message(message, user_message, is_private):
     try:
         response = responses.get_response(user_message)
         # await message.author.send(response) if is_private and message.channel != 'bot-alert' else await message.channel.send(response)
-        if response is not None:
+        if response is not None and is_private:
+            await message.author.send(response)
+        elif response is not None:
             await message.channel.send(response)
         else:
             pass
@@ -15,6 +17,7 @@ async def send_message(message, user_message, is_private):
         print(e)
 
 
+# Bot function
 def run_discord_bot():
     TOKEN = ''
     intents = discord.Intents.default()
@@ -36,10 +39,11 @@ def run_discord_bot():
 
         print(f'{username} said: "{user_message}" ({channel})')
 
+        # If message uses '?' at the beginning the bot will respond privately
         if user_message[0] == '?':
             user_message = user_message[1:]
             await send_message(message, user_message, is_private=True)
         else:
-            await send_message(message, user_message, is_private=True)
+            await send_message(message, user_message, is_private=False)
 
     client.run(TOKEN)
