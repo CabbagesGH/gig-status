4fb66851780ba5ffb8d20f74d70113300a0e057b
CabbagesGH <mtiff2009@gmail.com>
2023-12-09 15:05:26 +0000

    Added single port checking Removed server vars Added server JSON array

--- venv/responses.py
+++ venv/responses.py
@@ -1,5 +1,5 @@
-import random
 import socket
+import re
 
 
 def check_udp_port(domain, udp_port):
@@ -7,14 +7,14 @@ def check_udp_port(domain, udp_port):
 
     Args:
         domain: The domain name or IP address to check.
-        port: The port number to check.
+        udp_port: The port number to check.
 
     Returns:
         True if the port is open, False otherwise.
     """
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.settimeout(1)
+    sock.settimeout(0.4)
     try:
         sock.sendto(b"", (domain, udp_port))
         data, addr = sock.recvfrom(1024)
@@ -30,7 +30,7 @@ def check_tcp_port(domain, tcp_port):
 
     Args:
         domain: The domain name or IP address to check.
-        port: The port number to check.
+        tcp_port: The port number to check.
 
     Returns:
         True if the port is open, False otherwise.
@@ -46,18 +46,38 @@ def check_tcp_port(domain, tcp_port):
         sock.close()
 
 
-cabbages_url = 'cabbages.servegame.com'
-skillz_url = 'gig.illskillz.pro'
-anhur_url = 'anhur.servegame.com'
-syco_url = 'syco.servegame.com'
-tcp_port80 = 80
-tcp_port8081 = 8081
-udp_port7777 = 7777
-udp_port7778 = 7778
-udp_port7779 = 7779
-udp_port7780 = 7780
+# Gigantic Servers JSON
+servers = {
+    'cabbages': {
+        'url': 'cabbages.servegame.com',
+        'udp_ports': [7777, 7778],
+        'tcp_port': 80
+    },
+    'skillz': {
+        'url': 'gig.illskillz.pro',
+        'udp_ports': [7777],
+        'tcp_port': 80
+    },
+    'anhur': {
+        'url': 'anhur.servegame.com',
+        'udp_ports': [7777, 7778],
+        'tcp_port': 80
+    },
+    'syco': {
+        'url': 'syco.servegame.com',
+        'udp_ports': [7777, 7778, 7779, 7780],
+        'tcp_port': 8081
+    }
+}
+
+# Simplifies some later code
+cabbages_url = servers.get('cabbages')['url']
+skillz_url = servers.get('skillz')['url']
+anhur_url = servers.get('anhur')['url']
+syco_url = servers.get('syco')['url']
 
 
+# Checks the status of all Gigantic servers and available instances
 def check_server_status(server_url, tcp_port, udp_ports):
     web_status = 'running :bulb:' if check_tcp_port(server_url, tcp_port) else 'unavailable :wrench:'
 
@@ -69,36 +89,50 @@ def check_server_status(server_url, tcp_port, udp_ports):
     for status in port_status:
         if status == 'down :x:' and web_status == 'running :bulb:':
             down_count += 1
-        else:
-            down_count = 0
 
     return web_status, port_status, down_count
 
 
-cabbages_web, cabbages_ports, cabbages_count = check_server_status(
-    cabbages_url, tcp_port80, [udp_port7777, udp_port7778]
-)
+def check_port_status(server_name, port_number):
+    server_data = servers.get(server_name)
+    if server_data is None:
+        return 'Invalid server name'
 
-skillz_web, skillz_ports, skillz_count = check_server_status(
-    skillz_url, tcp_port80, [udp_port7777, udp_port7778]
-)
+    server_url = server_data['url']
+    udp_ports = server_data['udp_ports']
 
-anhur_web, anhur_ports, anhur_count = check_server_status(
-    anhur_url, tcp_port80, [udp_port7777, udp_port7778]
-)
+    if port_number not in udp_ports:
+        return 'Invalid port number for this server'
 
-syco_web, syco_ports, syco_count = check_server_status(
-    syco_url, tcp_port8081, [udp_port7777, udp_port7778, udp_port7779, udp_port7780]
-)
+    return '{} {}: This instance is running'.format(server_name.capitalize(), port_number) if check_udp_port(server_url, port_number) else '{} {}: This instance is available'.format(server_name.capitalize(), port_number)
 
 
+# Responses
 def get_response(message: str) -> str:
     p_message = message.lower()
 
+    # Extract server name and port number from the user's input
+    match = re.search(r'!status\s+([\w\-]+)\s+(\d+)', p_message)
+
     if p_message == '!status':
+        cabbages_web, cabbages_ports, cabbages_count = check_server_status(
+            servers.get('cabbages')['url'], servers.get('cabbages')['tcp_port'], servers.get('cabbages')['udp_ports']
+        )
+
+        skillz_web, skillz_ports, skillz_count = check_server_status(
+            servers.get('skillz')['url'], servers.get('skillz')['tcp_port'], servers.get('skillz')['udp_ports']
+        )
+
+        anhur_web, anhur_ports, anhur_count = check_server_status(
+            servers.get('anhur')['url'], servers.get('anhur')['tcp_port'], servers.get('anhur')['udp_ports']
+        )
+
+        syco_web, syco_ports, syco_count = check_server_status(
+            servers.get('syco')['url'], servers.get('syco')['tcp_port'], servers.get('syco')['udp_ports']
+        )
         return (('>>> ## Gigantic Server Status'
                  '\n\n**EU3** [Cabbages\'s Server](http://{}) :flag_eu: | The Web UI is {} and **{}/2** instances are available'
-                 '\n\n**EU2** [Skillz\'s Server](http://{}) :flag_eu: | The Web UI is {} and **{}/2** instances are available'
+                 '\n\n**EU2** [Skillz\'s Server](http://{}) :flag_eu: | The Web UI is {} and **{}/1** instances are available'
                  '\n\n**NA3** [Anhur\'s Server](http://{}) :flag_us: | The Web UI is {} and **{}/2** instances are available'
                  '\n\n**NA5** [Syco\'s Server](http://{}) :flag_us: | The Web UI is {} and **{}/4** instances are available')
                 .format(cabbages_url, cabbages_web, cabbages_count,
@@ -106,13 +140,13 @@ def get_response(message: str) -> str:
                         anhur_url, anhur_web, anhur_count,
                         syco_url, syco_web, syco_count,))
 
-    if p_message == 'hello':
-        return 'Hey there!'
-
-    if message == 'roll':
-        return str(random.randint(1, 6))
-
     if p_message == '!status help':
-        return '`This is a help message that you can modify.`'
+        return 'Enter `!status` to bring up  `This is a help message that you can modify.`'
 
-    return 'I didn\'t understand what you wrote. Try typing `!status help`.'
+    if match:
+        server_name = match.group(1)
+        port_number = int(match.group(2))
+
+        return check_port_status(server_name, port_number)
+    else:
+        return 'Invalid command. Use !status <server_name> <instance_number>'
